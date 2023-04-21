33c7da4f7e2a1f6ca248aa3db7fc4415cdffb1d4
ErnestX <xiangspecial2012@gmail.com>
2023-04-21 14:41:03 -0400

    Use the right input files and create edd, rdd files

---
+++ README.md
@@ -0,0 +1,12 @@
+Use the DesignBuilder's example file: under floor heating example
+
+Make sure energy plus 9.4 is selected in tools -> program options
+
+Modify the design builder file as needed
+
+File -> export -> energy plus -> simulation
+
+In export options: 
+	A lot of options to choose from! 
+	In Output tab, Miscellaneous, check EDD/RDD file if needed (slightly slows down the simulation) 
+	
--- test.py
+++ test.py
@@ -3,14 +3,20 @@ import os
 from eppy import modeleditor
 from eppy.modeleditor import IDF
 from pyenergyplus.api import EnergyPlusAPI
+# from pyenergyplus.datatransfer import DataExchange
 
-#iddfile = "C:/EnergyPlusV9-4-0/Energy+.idd" 
-iddPath = "C:/EnergyPlusV9-5-0/Energy+.idd" 
+# iddPath = "C:/EnergyPlusV9-5-0/Energy+.idd" 
+iddPath = "C:/EnergyPlusV9-4-0/Energy+.idd" 
 
-#idfname = "C:/Users/Eppy/Documents/IDFs/ASHRAE901_OfficeSmall_STD2019_NewYork.idf"
-idfPath = "C:/Users/Eppy/Documents/IDFs/US+MF+CZ4A+elecres+unheatedbsmt+IECC_2021.idf"
+idfPath = "C:/Users/Eppy/Documents/IDFs/UnderFloorHeatingPreset.idf"
+# idfPath = "C:/Users/Eppy/Documents/IDFs/IECC_OfficeSmall_STD2018_SanDiego.idf"
+# idfPath = "C:/Users/Eppy/Documents/IDFs/ASHRAE901_OfficeSmall_STD2019_SanDiego.idf"
+# idfPath = "C:/Users/Eppy/Documents/IDFs/ASHRAE901_OfficeSmall_STD2019_NewYork.idf"
+# idfPath = "C:/Users/Eppy/Documents/IDFs/US+MF+CZ3C+elecres+heatedbsmt+IECC_2021.idf"
+# idfPath = "C:/Users/Eppy/Documents/IDFs/US+MF+CZ4A+elecres+unheatedbsmt+IECC_2021.idf"
 
-epwPath = "C:/Users/Eppy/Documents/WeatherFiles/USA_MA_Boston-Logan.Intl.AP.725090_TMY3.epw"
+epwPath = "C:/Users/Eppy/Documents/WeatherFiles/USA_CA_San.Diego-Lindbergh.Field.722900_TMY3.epw"
+# epwPath = "C:/Users/Eppy/Documents/WeatherFiles/USA_MA_Boston-Logan.Intl.AP.725090_TMY3.epw"
 
 outputDir = os.path.dirname(idfPath)  + '/output'
 
@@ -29,7 +35,7 @@ idf = IDF(idfPath, epwPath)
 #     'output_prefix':os.path.basename(fname).split('.')[0],
 #     # 'output_suffix':'C',
 #     'output_directory':os.path.dirname(fname)  + '/output',
-#     # 'readvars':True,
+#     'readvars':True,
 #     # 'expandobjects':True
 #     }
 # idf.run(**options)
@@ -37,8 +43,13 @@ idf = IDF(idfPath, epwPath)
 # =================================================
 
 energyplus_api = EnergyPlusAPI()
+
+# dataExchange = energyplus_api.exchange
+# dataExchange.get_variable_handle()
+
 selfenergyplus_state = energyplus_api.state_manager.new_state()
 runtime = energyplus_api.runtime
 
-results = runtime.run_energyplus(selfenergyplus_state, ['-d', outputDir, '-w', epwPath, idfPath])
-print(results)
+exitCode = runtime.run_energyplus(selfenergyplus_state, ['-d', outputDir, '-w', epwPath, idfPath])
+print("exit code (zero is success): " + str(exitCode))
+
