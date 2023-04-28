d972c3bda39031ffdbab72c477e7932c6ab839c0
ErnestX <xiangspecial2012@gmail.com>
2023-04-28 14:30:05 -0400

    test out continuous action space: SAC did not produce optimal solution in simple test case

--- evaluate.py
+++ evaluate.py
@@ -3,13 +3,15 @@ import numpy as np
 import gymnasium as gym
 from GymEnvironment import Environment
 
-from stable_baselines3 import PPO
 from stable_baselines3 import DQN
-from stable_baselines3.ppo import MlpPolicy
+from stable_baselines3 import PPO
+from stable_baselines3 import A2C
+from stable_baselines3 import SAC
+
 from stable_baselines3.common.evaluation import evaluate_policy
 
 SAVE_PATH = "C:/Users/Eppy/Documents/CarbonControlRL/Models/TrainedModel"
-modelToEval = PPO.load(SAVE_PATH)
+modelToEval = SAC.load(SAVE_PATH)
 
 evalEnvironment = Environment()
 mean_reward, std_reward = evaluate_policy(modelToEval, evalEnvironment, n_eval_episodes=1)
--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import numpy as np
 import gymnasium as gym
 from GymEnvironment import Environment
 
+from stable_baselines3 import DQN # unstable
 from stable_baselines3 import PPO
-from stable_baselines3 import DQN
+from stable_baselines3 import A2C # chose the higher setpoint in test
+from stable_baselines3 import SAC # much, much slower then PPO (multiple actors); did not produce optimal policy
 
 SAVE_PATH = "C:/Users/Eppy/Documents/CarbonControlRL/Models/TrainedModel"
 
 environment = Environment()
-model = PPO("MlpPolicy", environment, verbose=2, gamma=1.0) # gamma: discount factor
+model = SAC("MlpPolicy", environment, verbose=2, gamma=1.0) # gamma: discount factor
 model.learn(total_timesteps=16384) 
 
 print("done learning ***************************************")
