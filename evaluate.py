4077d3912d67f0123ea074be9a84f8eb377270af
ErnestX <xiangspecial2012@gmail.com>
2023-04-27 23:53:52 -0400

    trying to save and eval model

---
+++ evaluate.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+import gymnasium as gym
+from GymEnvironment import Environment
+
+from stable_baselines3 import PPO
+from stable_baselines3.ppo import MlpPolicy
+from stable_baselines3.common.evaluation import evaluate_policy
+
+SAVE_PATH = "C:/Users/Eppy/Documents/CarbonControlRL/Models/TrainedModel"
+model = PPO.load(SAVE_PATH)
+
+evalEnvironment = Environment()
+mean_reward, std_reward = evaluate_policy(model, evalEnvironment, n_eval_episodes=100)
+print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import gymnasium as gym
 from GymEnvironment import Environment
@@ -7,15 +8,20 @@ from stable_baselines3.ppo import MlpPolicy
 from stable_baselines3.common.evaluation import evaluate_policy
 # from stable_baselines3.common.env_checker import check_env
 
+SAVE_PATH = "C:/Users/Eppy/Documents/CarbonControlRL/Models/TrainedModel"
+
 
 environment = Environment()
 model = PPO(MlpPolicy, environment, verbose=2)
 model.learn(total_timesteps=16384) #100000)
 
 print("done learning **********************************************")
-environment.close()
-print("testing model ************************************************")
+model.save(SAVE_PATH)
+print("model saved *****************************************")
 
-evalEnvironment = Environment()
-mean_reward, std_reward = evaluate_policy(model, evalEnvironment, n_eval_episodes=100)
-print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
+# environment.close()
+# print("testing model ************************************************")
+
+# evalEnvironment = Environment()
+# mean_reward, std_reward = evaluate_policy(model, evalEnvironment, n_eval_episodes=100)
+# print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
